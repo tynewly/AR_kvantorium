080e2ad98574e5d83c3b786deaffbe03dca94ebc
tynewly <akochkina260@gmail.com>
2026-03-06 19:59:11 +0300

    таймер

--- Assets/Scripts/question.cs
+++ Assets/Scripts/question.cs
@@ -4,19 +4,50 @@ public class question : MonoBehaviour
 {
     public Button[] buttonWrong;
     public Button correctButton;
+    public GameObject win;
+    public GameObject lose;
     private Color color;
+    public float time = 30;
+    private bool answer = false;
+  
+
+
+
     void Start()
     {
+        win.SetActive(false);
+        lose.SetActive(false);
+      
 
     }
+    public void timeAnswer()
+    {
+        if (answer == false)
+        {
+            time = time - Time.deltaTime;
+            if (time < 0)
+            {
+                wrongAnswer();
+
+            }
+
+            Debug.Log(time);
+        }
+            
+
+        
+    }    
     public void wrongAnswer()
     {
         color = Color.red;
         for (int i= 0; i < buttonWrong.Length; i++)
         {
             buttonWrong[i].GetComponent<Image>().color = color;
+
         }
         correctButton.GetComponent<Image>().color = Color.green;
+        lose.SetActive(true);
+        answer = true;
     }
     public void correctAnswer()
     {
@@ -26,10 +57,13 @@ public class question : MonoBehaviour
         {
             buttonWrong[i].GetComponent<Image>().color = Color.red;
         }
+        win.SetActive(true);
+        answer = true;
+
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        timeAnswer();
     }
 }
