e89764817d5e9da3cd122ac09ae1f51145da90bc
tynewly <akochkina260@gmail.com>
2026-03-17 17:36:44 +0300

    разработка квеста в процессе

--- Assets/Scripts/quest1.cs
+++ Assets/Scripts/quest1.cs
@@ -5,10 +5,12 @@ public class quest1 : MonoBehaviour
 {
     public TextMeshProUGUI Text;
     public GameObject Canvas_quest;
+    public GameObject Canvas_quiz;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Canvas_quest.SetActive(false);
+        Canvas_quiz.SetActive(false);
     }
 
     public void correctTarget()
@@ -18,7 +20,17 @@ public class quest1 : MonoBehaviour
 
     public void button1()
     {
-        Text.text = "knkinkj";
+        Text.text = "Вы испугались и решили откупиться. Отдайте 1 любую единицу товара";
+    }
+
+    public void button2()
+    {
+        Text.text = "Вы не повелись на провокации. Бросьте кубик, он определит исход драки";
+    }
+
+    public void button3()
+    {
+        Text.text = "Противники оказались слишком сильны, вернитесь на 2 шага назад";
     }
 
 
