07e6210af25e60fc5cd04bcaabd915392feff9f1
tynewly <akochkina260@gmail.com>
2026-04-15 16:59:16 +0300

    перевод старославянского_продолжение

--- Assets/Scripts/Russian_ancient_script.cs
+++ Assets/Scripts/Russian_ancient_script.cs
@@ -5,14 +5,27 @@ using UnityEngine.UI;
 public class Russian_ancient_script : MonoBehaviour
 {
     public GameObject Canvas;
+    public GameObject image_chronicle;
+    public GameObject text_chronicle;
+    public GameObject text_down;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Canvas.SetActive(false);    
+           
     }
     public void Target_found()
     {
         Canvas.SetActive(true);
+        image_chronicle.SetActive(true);
+        text_chronicle.SetActive(false);
+        text_down.SetActive(true);
+    }
+    public void chronicle_touch()
+    {
+        image_chronicle.SetActive(true);
+        text_chronicle.SetActive(true);
+        text_down.SetActive(true);
+
     }
 
     // Update is called once per frame
