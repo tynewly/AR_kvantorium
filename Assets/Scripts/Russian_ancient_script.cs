4bb3ffce339a71ab48b78c9bb5f389cafe02bb16
tynewly <akochkina260@gmail.com>
2026-05-06 21:58:04 +0300

    Revert "исправлены ошибки, translate не работает"
    This reverts commit 0f899eabf52bcf47c66c15a862dea32ff9ee289a.

--- Assets/Scripts/Russian_ancient_script.cs
+++ Assets/Scripts/Russian_ancient_script.cs
@@ -5,7 +5,6 @@ using static System.Net.Mime.MediaTypeNames;
 using System.Threading;
 using Unity.VectorGraphics;
 using UnityEngine.SceneManagement;
-using System;
 
 public class Russian_ancient_script : MonoBehaviour
 {
@@ -21,9 +20,8 @@ public class Russian_ancient_script : MonoBehaviour
     public GameObject text_up;
     public TextMeshProUGUI text_up2;
     public TMP_InputField inputField;
-    public GameObject inputField1;
-    public GameObject confimButton;
-    public GameObject exit;
+    public GameObject inputField1;    
+
     private int count = 0;
     private string[] correctSeas = { "Каспийское", "Аравийское", "Чёрное" };
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -37,8 +35,7 @@ public class Russian_ancient_script : MonoBehaviour
     public void saveText()
     {
         string userText = inputField.text;
-        char[] separators = new char[] { ' ', ',', '.', ':', ';'};
-        string[] words = userText.Split(separators, System.StringSplitOptions.RemoveEmptyEntries);
+        string[] words = userText.Split(' ');
         Debug.Log(words.Length);
         if (words.Length == 3)
         {
@@ -90,15 +87,8 @@ public class Russian_ancient_script : MonoBehaviour
         //ariation_3.SetActive(false);
         text_up.SetActive(true);
         inputField1.SetActive(false);
-        confimButton.SetActive(false);
-        exit.SetActive(false);
 
 
-
-    }
-    public void ExitTouch()
-    {
-        Canvas.SetActive(false);
     }
     public void book_touch()
     {
@@ -108,16 +98,12 @@ public class Russian_ancient_script : MonoBehaviour
         text_up.SetActive(false);
         text_down2.text = "Нажмите на записи, чтобы прочитать";
         inputField1.SetActive(false);
-        confimButton.SetActive(false);
-        exit.SetActive(false);
     }
     public void chronicle_touch()
     {
         text_chronicle.SetActive(true);
         text_down2.text = "Нажмите на надпись, чтобы выйти";
         text_up.SetActive(false);
-        confimButton.SetActive(false);
-        exit.SetActive(false);
     }
     public void textDown_touch()
     {
@@ -128,11 +114,9 @@ public class Russian_ancient_script : MonoBehaviour
         //variation_2.SetActive(true);
         //variation_3.SetActive(true);
         text_up.SetActive(true);
-        text_down.SetActive(false);
+        text_down2.text = "Нажмите на книгу, чтобы вспомнить";
         inputField1.SetActive(true);
         text_up2.text = "Через какие моря проходил путь Великого путешественника Афанасия Никитина?";
-        confimButton.SetActive(true);
-        exit.SetActive(true);
         //book.transform.position = new Vector3(-189f, 23f, 0f);
         //book.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 
