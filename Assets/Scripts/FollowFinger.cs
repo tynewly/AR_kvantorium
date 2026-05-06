0a661e70d6f56038ff0d3830db75925fa02e9c72
tynewly <akochkina260@gmail.com>
2026-05-06 20:38:31 +0300

    хз

--- Assets/Scripts/FollowFinger.cs
+++ Assets/Scripts/FollowFinger.cs
@@ -31,11 +31,16 @@ public class FollowFinger : MonoBehaviour
           // Touch это тип данных. Он типо берет инфу о пальце и сохраняет в переменную tоuch
             Glass.SetActive(true);
             Glass.transform.position = touch.position;
+<<<<<<< Updated upstream
             Debug.Log("работает");
+=======
+            Debug.Log("Работает");
+>>>>>>> Stashed changes
         }
         else
         {
-            Glass.gameObject.SetActive(false);
+            Glass.SetActive(false);
+            Debug.Log("Не работает");
         }
         
     }
